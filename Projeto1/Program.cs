59697e3870072e0bdc23e69b1950c69eee005ed0
douglasferreirag <douglas.ferreirag@gmail.com>
2024-08-19 20:57:25 -0300

    Projetos

---
+++ Projeto1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Projeto1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ﻿decimal salario, fgts;
+
+        Console.Write("Salário (R$)..: ");
+        salario = Convert.ToDecimal(Console.ReadLine());
+
+        fgts = (salario / 100) * 8;
+
+        Console.WriteLine($"\nFGTS: {fgts:C2}");
+    }
+}
---
+++ Projeto2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Projeto2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Insira o primeiro lado do triângulo: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Insira o segundo lado do triângulo: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Insira o terceiro lado do triângulo: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+        double semiperimetro = (a + b + c) / 2;
+        double dentroDaRaiz = semiperimetro * (semiperimetro - a) * (semiperimetro - b) * (semiperimetro - c);
+        double area = Math.Round(Math.Sqrt(dentroDaRaiz),2);
+        Console.WriteLine("A área é: " + area);
+    }
+}
