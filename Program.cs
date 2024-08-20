37d3e60637b703f017ed9fab4f1834abe55cb14a
douglasferreirag <douglas.ferreirag@gmail.com>
2024-08-19 21:03:29 -0300

    Projeto para exercitar operador ternário

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ParImpar;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número: ");
+
+        var numero = Console.ReadLine();
+
+        int restoDaDivisao = Convert.ToInt32(numero) % 2;
+
+        String parOuImpar = restoDaDivisao == 0 ? "par" : "ímpar";
+
+        Console.WriteLine("O número inserido é: " + parOuImpar);
+
+    }
+}
